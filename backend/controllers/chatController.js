c352a1e974dc38c97a6a4644a875ed9fffa89c70
abhishek61067 <abhishek.bhattarai61067@gmail.com>
2025-05-18 20:07:56 +0545

    feat: controllers for all the cruds of chat

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -72,4 +72,124 @@ const createChat = asyncHandler(async (req, res) => {
   res.status(201).json({ message: "Chat created successfully" });
 });
 
-export { createChat, readChat };
+const createGroupChat = asyncHandler(async (req, res) => {
+  if (!req.body.users || !req.body.name) {
+    return res.status(400).send({ message: "Please fill all the fields" });
+  }
+
+  let users = req.body.users;
+  if (typeof users === "string") {
+    users = JSON.parse(users);
+  }
+  if (!Array.isArray(users)) {
+    return res.status(400).send("Users must be an array");
+  }
+  if (users.length < 2) {
+    return res
+      .status(400)
+      .send("More than 2 users are required to form a group chat");
+  }
+  users.push(req.user._id);
+
+  try {
+    const groupChat = await Chat.create({
+      chatName: req.body.name,
+      users: users,
+      isGroupChat: true,
+      groupAdmin: req.user._id,
+    });
+
+    const fullGroupChat = await Chat.findOne({ _id: groupChat._id })
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password");
+
+    res.status(200).json(fullGroupChat);
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+});
+
+const renameGroupChat = asyncHandler(async (req, res) => {
+  const { chatId, chatName } = req.body;
+
+  const updatedChat = await Chat.findByIdAndUpdate(
+    chatId,
+    {
+      chatName: chatName,
+    },
+    {
+      new: true,
+    }
+  )
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password");
+
+  if (!updatedChat) {
+    res.status(404);
+    throw new Error("Chat Not Found");
+  } else {
+    res.json(updatedChat);
+  }
+});
+
+const addToGroup = asyncHandler(async (req, res) => {
+  const { chatId, userId } = req.body;
+
+  const added = await Chat.findByIdAndUpdate(
+    chatId,
+    {
+      $addToSet: { users: userId },
+    },
+    {
+      new: true,
+    }
+  )
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password");
+  if (!added) {
+    res.status(404);
+    throw new Error("Chat Not Found");
+  }
+  // Sort users by their createdAt field, newest first
+  added.users = added.users.sort(
+    (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+  );
+  res.json(added);
+});
+
+const removeFromGroup = asyncHandler(async (req, res) => {
+  const { chatId, userId } = req.body;
+
+  const removed = await Chat.findByIdAndUpdate(
+    chatId,
+    {
+      $pull: { users: userId },
+    },
+    {
+      new: true,
+    }
+  )
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password");
+
+  if (!removed) {
+    res.status(404);
+    throw new Error("Chat Not Found");
+  }
+  // Sort users by their createdAt field, newest first
+  removed.users = removed.users.sort(
+    (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+  );
+
+  res.json(removed);
+});
+
+export {
+  createChat,
+  readChat,
+  createGroupChat,
+  renameGroupChat,
+  addToGroup,
+  removeFromGroup,
+};
--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,13 +1,20 @@
 import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
-import { createChat, readChat } from "../controllers/chatController.js";
+import {
+  addToGroup,
+  createChat,
+  createGroupChat,
+  readChat,
+  removeFromGroup,
+  renameGroupChat,
+} from "../controllers/chatController.js";
 
 const router = express.Router();
 router.post("/", protect, createChat);
 router.get("/", protect, readChat);
-// router.post("/group", protect, createGroupChat);
-// router.put("/rename", protect, renameGroupChat);
-// router.put("/groupremove", protect, removeFromGroup);
-// router.put("/groupadd", protect, addToGroup);
+router.post("/group/", protect, createGroupChat);
+router.put("/group/rename", protect, renameGroupChat);
+router.put("/group/add", protect, addToGroup);
+router.put("/group/remove", protect, removeFromGroup);
 
 export const chatRoutes = router;
