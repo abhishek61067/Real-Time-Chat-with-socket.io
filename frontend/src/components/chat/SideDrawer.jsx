bc4230cae5ab5c13c5f4a08d59b0e385a5e8a1ab
abhishek61067 <abhishek.bhattarai61067@gmail.com>
2025-05-27 16:30:13 +0545

    feat: utilities for token

--- frontend/src/components/chat/SideDrawer.jsx
+++ frontend/src/components/chat/SideDrawer.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure,
+  Button,
+  Input,
+} from "@chakra-ui/react";
+import { useRef } from "react";
 
 const SideDrawer = () => {
-  return <div>SideDrawer</div>;
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = useRef();
+
+  return (
+    <>
+      <Button ref={btnRef} colorScheme="teal" onClick={onOpen}>
+        Open
+      </Button>
+      <Drawer
+        isOpen={isOpen}
+        placement="left"
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Create your account</DrawerHeader>
+
+          <DrawerBody>
+            <Input placeholder="Type here..." />
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Button variant="outline" mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme="blue">Save</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
 };
 
 export default SideDrawer;
